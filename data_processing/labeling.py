a0d37bc4548f38c81749990bb22b749727002a6a
AidenJONeil123 <106366655+AidenJONeil123@users.noreply.github.com>
2022-10-06 14:05:15 -0400

    Update labeling.py
    Got rid of renaming of files, chaned "good v bad" to "shift or no shift", organizes files into train and validation folders each with a shift and no_shift subfolder

--- data_processing/labeling.py
+++ data_processing/labeling.py
@@ -1,21 +1,19 @@
-# Company: UPrintWeFix
-# Author: Aiden J O'Neil
 
-# Usage: python labeling.py [FOLDER NAME] [TIME OF SHIFT]
+# Usage: python labeling.py [FOLDER NAME] [TIME OF SHIFT] [Valid OR Training]
 # Time of shift has format HH:MM:SS
 
 import sys, os, shutil, csv
 from pathlib import Path
 
-CSV_FILE_NAME = "labels.csv"
-
 # Command Line Inputs
 folder = sys.argv[1]
 shiftStart = sys.argv[2]
+valid_train = sys.argv[3]
+CSV_FILE_NAME = "_" + folder + "-labels.csv"
 
 # Create writeable CSV
 with open(folder + '/' + CSV_FILE_NAME, 'w', newline='\n') as csvfile:
-    fieldnames = ['FILE NAME', 'GOOD OR BAD?']
+    fieldnames = ['FILE NAME', 'SHIFT OR NO SHIFT?']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
 
@@ -29,18 +27,10 @@ with open(folder + '/' + CSV_FILE_NAME, 'w', newline='\n') as csvfile:
         fileMin = fileName[-9:-7]
         fileSec = fileName[-6:-4]
 
-        # Include for renaming of files
-        # data_file = Path(folder + '/' + fileName)
-        # good = fileName[:-4] + "_GOOD" + fileName[-4:]
-        # bad = fileName[:-4] + "_BAD" + fileName[-4:]
-
         # If image was taken after "shiftStart", it is bad. Otherwise, it is good
         if (fileHour > shiftStart[0:2]) or (fileHour == shiftStart[0:2] and fileMin > shiftStart[3:5]) or (fileMin == shiftStart[3:5] and fileSec > shiftStart[6:8]):
-            # data_file.rename(bad)
-            # shutil.move(bad, folder)
-            writer.writerow({"FILE NAME": fileName, "GOOD OR BAD?" : "BAD"})
+            # writer.writerow({"FILE NAME": fileName, "SHIFT OR NO SHIFT?" : "SHIFT"})
+            shutil.move(folder + '/' + fileName, valid_train + '/shift')
         else:
-            # data_file.rename(good)
-            # shutil.move(good, folder)
-            writer.writerow({"FILE NAME": fileName, "GOOD OR BAD?" : "GOOD"})
-            
+            # writer.writerow({"FILE NAME": fileName, "SHIFT OR NO SHIFT?" : "NO SHIFT"})
+            shutil.move(folder + '/' + fileName, valid_train + '/no_shift')
