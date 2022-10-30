62e99b44f394fe64f361d9c9684ea054854bedf5
palmtrey <palmtrey779@gmail.com>
2022-10-30 09:39:45 -0400

    Update train.py

--- model/train.py
+++ model/train.py
@@ -22,10 +22,10 @@ import os
 import copy
 from tqdm import tqdm
 
-DATA_DIR = 'data/phase_2_labeled'
+DATA_DIR = '../_data/phase_2_labeled'
 EPOCHS = 1
-OUTPUT_WEIGHTS_FILE = 'trained_model_phase2.pickle'
-
+OUTPUT_WEIGHTS_FILE = 'trained_model_phase2_reg1e-4.pickle'
+REG = 1e-4
 
 def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
     since = time.time()
@@ -36,6 +36,10 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
     for epoch in range(num_epochs):
         print(f'Epoch {epoch}/{num_epochs - 1}')
         print('-' * 10)
+
+        with open(OUTPUT_WEIGHTS_FILE + '.txt', 'a') as f:
+            f.write(f'\nEpoch {epoch}/{num_epochs - 1}')
+            f.write('\n' + '-' * 10)
 
         # Each epoch has a training and validation phase
         for phase in ['train', 'val']:
@@ -77,17 +81,24 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
             epoch_acc = running_corrects.double() / dataset_sizes[phase]
 
             print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
-
+            with open(OUTPUT_WEIGHTS_FILE + '.txt', 'a') as f:
+                f.write(f'\n{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
             # deep copy the model
             if phase == 'val' and epoch_acc > best_acc:
                 best_acc = epoch_acc
                 best_model_wts = copy.deepcopy(model.state_dict())
 
         print()
+        with open(OUTPUT_WEIGHTS_FILE + '.txt', 'a') as f:
+            f.write('\n')
 
     time_elapsed = time.time() - since
     print(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
     print(f'Best val Acc: {best_acc:4f}')
+
+    with open(OUTPUT_WEIGHTS_FILE + '.txt', 'a') as f:
+        f.write(f'\nTraining complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
+        f.write(f'\nBest val Acc: {best_acc:4f}')
 
     # load best model weights
     model.load_state_dict(best_model_wts)
@@ -95,11 +106,16 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
 
 
 if __name__ == '__main__':
+
+    # Initialize metadata file
+    with open(OUTPUT_WEIGHTS_FILE + '.txt', 'w') as f:
+        f.write('')
+
     cudnn.benchmark = True
     plt.ion()   # interactive mode
     #
     # Transforms for layer shifting
-    # 
+    #
     data_transforms = {
         'train': transforms.Compose([
             transforms.ToTensor(),
@@ -110,7 +126,7 @@ if __name__ == '__main__':
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ]),
     }
- 
+
 
     image_datasets = {x: datasets.ImageFolder(os.path.join(DATA_DIR, x),
                                             data_transforms[x])
@@ -124,6 +140,10 @@ if __name__ == '__main__':
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print('Device: ' + str(device))
+
+    with open(OUTPUT_WEIGHTS_FILE + '.txt', 'a') as f:
+        f.write('\nClasses: ' + str(class_names))
+        f.write('\nDevice: ' + str(device))
 
     # Get a batch of training data
     inputs, classes = next(iter(dataloaders['train']))
@@ -146,11 +166,12 @@ if __name__ == '__main__':
     criterion = nn.CrossEntropyLoss()
 
     # Observe that all parameters are being optimized
-    optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+    optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9, weight_decay=REG)
 
     # Decay LR by a factor of 0.1 every 7 epochs
     exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
 
+    
 
     model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
                        num_epochs=EPOCHS)
