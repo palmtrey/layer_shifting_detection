63236a88f2c19796fe63911e570a3d0eb3c47779
palmtrey <52005788+palmtrey@users.noreply.github.com>
2022-10-31 14:33:23 -0400

    Create splitmp4.py

---
+++ processing/splitmp4.py
@@ -0,0 +1,27 @@
+import cv2
+import os
+
+
+INPUT_MP4 = '../_data/mp4s/correct_phil_28m_20221031144221-fail.mp4'
+OUTPUT_DIR = '../_data/adhesion_phil_ECEender3v2_cam0_Oct31_1'
+
+if not os.path.isdir(OUTPUT_DIR):
+    os.mkdir(OUTPUT_DIR)
+
+capture = cv2.VideoCapture(INPUT_MP4)
+ 
+frameNr = 0
+
+while (True):
+ 
+    success, frame = capture.read()
+ 
+    if success:
+        cv2.imwrite(os.path.join(OUTPUT_DIR, str(frameNr) + '.jpg'), frame)
+ 
+    else:
+        break
+ 
+    frameNr += 1
+ 
+capture.release()
