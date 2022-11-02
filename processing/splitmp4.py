43127bacfc20f1733de47ea18e44d849fe52185e
palmtrey <52005788+palmtrey@users.noreply.github.com>
2022-11-02 13:41:29 -0400

    Update splitmp4.py

--- processing/splitmp4.py
+++ processing/splitmp4.py
@@ -2,9 +2,9 @@ import cv2
 import os
 
 
-INPUT_MP4 = '../_data\mp4s\Z1.8-Y--4.26_20221031190036.mp4'
+INPUT_MP4 = '../_data/mp4s/correct_phil_28m_20221031180938.mp4'
 OUTPUT_DIR = '../_data'
-OUTPUT_FOLDER = 'ender_0'
+OUTPUT_FOLDER = 'ender_2'
 
 
 if not os.path.isdir(OUTPUT_DIR):
@@ -15,7 +15,7 @@ capture = cv2.VideoCapture(INPUT_MP4)
 frameNr = 0
 
 while (True):
- 
+    print('frame')
     success, frame = capture.read()
  
     if success:
