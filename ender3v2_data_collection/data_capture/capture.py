d40465b70074274f3ebf53dd1323465eb16d2df2
AidenJONeil123 <106366655+AidenJONeil123@users.noreply.github.com>
2022-11-17 14:04:14 -0500

    Update capture.py

--- ender3v2_data_collection/data_capture/capture.py
+++ ender3v2_data_collection/data_capture/capture.py
@@ -1,7 +1,7 @@
 from datetime import datetime
-import os
 from paramiko import SSHClient
 from scp import SCPClient
+import os
 
 OUTPUT_PATH = './correct_clip_small_2_oct20_0'
 CAP_INTERVAL_MS = 10000
@@ -16,13 +16,11 @@ ssh.connect('aiden@128.153.28.135')
 scp = SCPClient(ssh.get_transport())
 
 for i in range(220):
-    now = datetime.now()
-    dtstr = now.strftime("%Y-%m-%d_%H_%M_%S")
-
-    os.system('libcamera-jpeg -o ' + dtstr + '.jpg -t ' + str(CAP_INTERVAL_MS) + ' --width 3280 --height 2464 --gain 4 > /dev/null 2>&1')
-    os.system('mv ' + dtstr + '.jpg ' + OUTPUT_PATH)
-    print('Image captured.')
-
+	now = datetime.now()
+	dtstr = now.strftime("%Y-%m-%d_%H_%M_%S")
+	os.system('libcamera-jpeg -o ' + dtstr + '.jpg -t ' + str(CAP_INTERVAL_MS) + ' --width 3280 --height 2464 --gain 4 > /dev/null 2>&1')
+	os.system('mv ' + dtstr + '.jpg ' + OUTPUT_PATH)
+	print('Image captured.')
 	src = path.join(OUTPUT_PATH, dtstr + '.jpg')
 	des = path.join(images, dtstr + '.jpg')
 	scp.put(src, des)
