6841a4ce4d37cb76055a0b8c9400babca1f999b5
swanny41 <72165352+swanny41@users.noreply.github.com>
2022-11-22 13:12:08 -0500

    updated capture

--- capture/capture.py
+++ capture/capture.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 import os
 
-
 OUTPUT_PATH = './correct_clip_small_2_oct20_0'
 CAP_INTERVAL_MS = 10000
 
@@ -9,11 +8,11 @@ CAP_INTERVAL_MS = 10000
 if not os.path.isdir(OUTPUT_PATH):
 	os.mkdir(OUTPUT_PATH)
 
-
 for i in range(220):
-    now = datetime.now()
-    dtstr = now.strftime("%Y-%m-%d_%H_%M_%S")
-
-    os.system('libcamera-jpeg -o ' + dtstr + '.jpg -t ' + str(CAP_INTERVAL_MS) + ' --width 3280 --height 2464 --gain 4 > /dev/null 2>&1')
-    os.system('mv ' + dtstr + '.jpg ' + OUTPUT_PATH)
-    print('Image captured.')
+	now = datetime.now()
+	dtstr = now.strftime("%Y-%m-%d_%H_%M_%S")
+	os.system('libcamera-jpeg -o ' + dtstr + '.jpg -t ' + str(CAP_INTERVAL_MS) + ' --width 3280 --height 2464 --gain 4 > /dev/null 2>&1')
+	os.system('mv ' + dtstr + '.jpg ' + OUTPUT_PATH)
+	print('Image captured.')
+	src = path.join(OUTPUT_PATH, dtstr + '.jpg')
+	os.system('sshpass -p uprintwefix1234 scp ' + src + 'oneilaj@128.153.28.135://home/images')
